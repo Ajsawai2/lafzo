223337304df93a30a68e067ec1c451110d559c69
Abhishek <bygt20@gmail.com>
2025-12-31 10:35:03 +0530

    fix home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import MessageCard from "@/components/MessageCard"
+import MessageCard from "@/components/MessageCard";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="pt-20 pb-32 px-4">
       <MessageCard />
     </main>
-  )
+  );
 }
